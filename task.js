48a074b616b9e654ee597f5a2f4d9b82878f7fbf
Danil Krutikov <kdanil.i@yandex.ru>
2023-12-25 20:18:38 +0700

    Initial cimmit

--- task.js
+++ task.js
@@ -4,9 +4,17 @@ xhr.open("get", "https://students.netoservices.ru/nestjs-backend/slow-get-course
 xhr.responseType = "json"
 xhr.send()
 xhr.onload = function () {
-      const data = xhr.response.response.Valute
-      document.getElementById("loader").classList.remove("loader_active");
-      for (let i in data) {
-        item.insertAdjacentHTML('beforeEnd', `<div class="item"><div class="item__code">${data[i].CharCode}</div><div class="item__value">${ data[i].Value}</div><div class="item__currency">руб.</div></div>`)
-        }
+  if (xhr.status == 200) {
+    const data = xhr.response.response.Valute
+    document.getElementById("loader").classList.remove("loader_active");
+    for (let i in data) {
+      item.insertAdjacentHTML('beforeEnd', `<div class="item"><div class="item__code">${data[i].CharCode}</div><div class="item__value">${ data[i].Value}</div><div class="item__currency">руб.</div></div>`)
+    };
+  } else if (xhr.status == 404) {
+    console.log("Запрашиваемый ресурс не найден!")
+  } else if (xhr.status == 500) {
+    console.log("Ошибка на стороне сервера!")
+  } else {
+    console.log("Запрос не удался")
+  }
 }
